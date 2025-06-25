cd9db757640463ea8f8bd3f193a3db01608d7c67
CodingKatsa <167198175+CodingKatsa@users.noreply.github.com>
2025-06-25 15:04:20 +0100

    Updated string in JV

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function generatePoem(event) {
     event.preventDefault();
 
-    new Typewriter('#poem', {
-        strings: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+    new Typewriter("#poem", {
+        strings: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         autoStart: true,
         delay: 1,
         cursor: "",
