3a1f32389e760706ea0e5de43268aeb3e44f105c
CodingKatsa <167198175+CodingKatsa@users.noreply.github.com>
2025-06-29 14:08:06 +0100

    Improved CSS

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function displayPoem(response){
             
         let apiKey = "304aba2f3bf848o9c4ec2t268c006e59";
         let prompt = `Generate a poem for a birthday person about ${instructionsInput.value}`
-        let context = "You are a creative poem expert and love to write short and funny poems. Your mission is to generate a poem, which rhymes, with no more than 4 lines with basic HMTL. Do not include a title to the poem and signt the poem with 'SheCodes AI' inside a <strong> element. Make sure to follow the user instructions";
+        let context = "You are a creative poem expert and love to write short and funny poems. Your mission is to generate a poem, which rhymes, with no more than 4 lines with basic HMTL. Do not include a title to the poem and signt the poem with 'SheCodes AI' inside a <strong> element <font-size: 2px>. Make sure to follow the user instructions";
         let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
 let poemElement = document.querySelector("#poem");
