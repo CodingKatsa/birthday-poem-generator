f721d776d6681c21c3477e06ecb4e5d5764d1474
CodingKatsa <167198175+CodingKatsa@users.noreply.github.com>
2025-06-29 14:30:45 +0100

    Improved final details and updated links

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayPoem(response){
-    new Typewriter("#poem", {
+function displayRhyme(response){
+    new Typewriter("#rhyme", {
         strings: response.data.answer,
         autoStart: true,
         delay: 1,
@@ -7,24 +7,24 @@ function displayPoem(response){
       });
 
         }
-    function generatePoem(event) {
+    function generateRhyme(event) {
         event.preventDefault();
 
         let instructionsInput = document.querySelector ("#user-instructions");
             
         let apiKey = "304aba2f3bf848o9c4ec2t268c006e59";
-        let prompt = `Generate a poem for a birthday person about ${instructionsInput.value}`
-        let context = "You are a creative poem expert and love to write short and funny poems. Your mission is to generate a poem, which rhymes, with no more than 4 lines with basic HMTL. Do not include a title to the poem and signt the poem with 'SheCodes AI' inside a <strong> element <font-size: 2px>. Make sure to follow the user instructions";
+        let prompt = `Generate a rhyme for a birthday person about ${instructionsInput.value}`
+        let context = "You are a creative rhyming expert and love to write short and funny rhymes. Your mission is to generate a poem which rhymes, with no more than 4 lines with basic HMTL. Do not include a title to the rhyme and sign the rhyme with 'SheCodes AI' inside a <strong> element <font-size: 2px>. Make sure to follow the user instructions";
         let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
-let poemElement = document.querySelector("#poem");
-        poemElement.classList.remove ("hidden");
-        poemElement.innerHTML=`<div class = "generating">⏳ Generating a birthday poem about ${instructionsInput.value}</div>`
+let rhymeElement = document.querySelector("#rhyme");
+        rhymeElement.classList.remove ("hidden");
+        rhymeElement.innerHTML=`<div class = "generating">⏳ Generating a birthday rhyme about ${instructionsInput.value}</div>`
 
-        axios.get(apiURL).then(displayPoem);
+        axios.get(apiURL).then(displayRhyme);
 
     }
 
   
-  let poemFormElement = document.querySelector("#poem-generator");
-  poemFormElement.addEventListener("submit", generatePoem);
+  let rhymeFormElement = document.querySelector("#rhyme-generator");
+  rhymeFormElement.addEventListener("submit", generateRhyme);
